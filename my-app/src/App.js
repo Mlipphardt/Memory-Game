5e83721841a980b2af8893cf58b4389330d600be
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-23 16:11:31 -0400

    Added function to shuffle characters using Fisher-Yates method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,13 +11,39 @@ class App extends Component {
     topScore: 0,
   };
 
+  //Checks if new top score has been reached when score updates.
   componentDidUpdate() {
     if (this.state.score > this.state.topScore) {
       this.setState({ topScore: this.state.score });
     }
   }
 
+  //Fisher-Yates algorithim for shuffling cards.
+  shuffleArray(array) {
+    //Variables to save current index, save a random index, and transfer numbers.
+    let currentIndex = array.length,
+      rolledIndex,
+      tempHolder;
+
+    //Will end when no numbers left to shuffle.
+    while (currentIndex) {
+      //Rolls to pick a random unshuffled item.
+      rolledIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      //Back of array item saved.
+      tempHolder = array[currentIndex];
+      //Randomly rolled item placed in back of array.
+      array[currentIndex] = array[rolledIndex];
+      //Former rolled index now becomes the number formerly in the back of the unshuffled array.
+      array[rolledIndex] = tempHolder;
+    }
+    //Return the shuffled array.
+    return array;
+  }
+
   handleClick = (id) => {
+    this.setState({ characters: this.shuffleArray(characters) });
+    //let shuffled = this.shuffleArray(shuffleReady);
     console.log("You clicked a button! ID: " + id);
     if (!this.state.clicked.includes(id)) {
       this.setState((state) => ({
