33421079bb9d99613bc0beb590aa54955994f9ba
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-27 13:56:02 -0400

    Updates

---
+++ precache-manifest.681107fc1d45a6afdf23d0cd7b563cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd36132c9b25497e5a9194afe5988bc",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "57bbe5cdeb292ac1ed2a",
+    "url": "/Memory-Game/static/css/main.2bff0bc0.chunk.css"
+  },
+  {
+    "revision": "b0d886aa6068b416d20e",
+    "url": "/Memory-Game/static/js/2.b27b9a6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Memory-Game/static/js/2.b27b9a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57bbe5cdeb292ac1ed2a",
+    "url": "/Memory-Game/static/js/main.fc5f3ccf.chunk.js"
+  },
+  {
+    "revision": "d2dcb393c0b3d979b79f",
+    "url": "/Memory-Game/static/js/runtime-main.5cba8c8d.js"
+  }
+]);
